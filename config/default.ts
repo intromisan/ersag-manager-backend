c120975b59685138827b09eaa3dc22520bf728d6
Serge Najimov <najimovserge@gmail.com>
2022-05-24 21:13:59 +0200

    added .env file

---
+++ config/default.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT || 5001,
+  host: process.env.HOST,
+  dbUri: process.env.DB_URI,
+  saltWorkFactor: process.env.SALT_WORK_FACTOR,
+  accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
+  refreshTokenTtl: process.env.REFRESH_TOKEN_TTL,
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY
+};
